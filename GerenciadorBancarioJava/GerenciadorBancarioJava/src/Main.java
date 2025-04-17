15ec7e95394097ee84bcfe37d1c1ba21fdde92ba
William Mateus <williammnovais@gmail.com>
2025-04-16 21:09:14 -0300

    Criacao da funcao de saques

--- GerenciadorBancarioJava/GerenciadorBancarioJava/src/Main.java
+++ GerenciadorBancarioJava/GerenciadorBancarioJava/src/Main.java
@@ -6,43 +6,43 @@ public class Main {
         ArrayList<ArrayList> acounts = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         boolean isOn = true;
-        while (isOn){
+        while (isOn) {
             System.out.println("Segue abaixo todas as funcoes do codigo \n" +
                     "0 - Sair do Programa\n" +
                     "1 - Cadastrar Contas\n" +
                     "2 - Realizar Depositos\n" +
-                    "3 - Consultar Saldo\n" +
-                    "4 - Consultar Historico de Movimentacoes\n" +
-                    "5 - Transferir Entre Contas \n" +
+                    "3 - Realizar Saques\n" +
+                    "4 - Consultar Saldo\n" +
+                    "5 - Consultar Historico de Movimentacoes\n" +
+                    "6 - Transferir Entre Contas \n" +
                     "Digite o Numero da funcao");
             int option = scanner.nextInt();
             scanner.nextLine();
-            switch (option){
+            switch (option) {
                 case 0:
                     isOn = false;
                     break;
                 case 1:
                     System.out.println("Digite Do Nome do Responsavel pela Conta");
                     String user = scanner.next();
-                    acounts.add(0,new ArrayList<String>());
-                    acounts.get(0).add(0,user);
-                    acounts.get(0).add(1,"0");
+                    acounts.add(0, new ArrayList<String>());
+                    acounts.get(0).add(0, user);
+                    acounts.get(0).add(1, "0");
 
                     break;
                 case 2:
                     int count = 0;
-                    for (ArrayList arrays:acounts){
-                        System.out.println("Numero da conta "+count+ " Nome do Responsavel pela conta "+ arrays.get(0));
+                    for (ArrayList arrays : acounts) {
+                        System.out.println("Numero da conta " + count + " Nome do Responsavel pela conta " + arrays.get(0));
                         count++;
                     }
                     System.out.println("Digite o Numero da conta do usuario que voce quer efetuar o deposito\n");
                     int numberAcaunt = scanner.nextInt();
-                    if ((numberAcaunt<0) || (numberAcaunt>=acounts.size())){
+                    if ((numberAcaunt < 0) || (numberAcaunt >= acounts.size())) {
                         System.out.println("O numero da conta que voce digitou nao costa");
                         break;
 
-                    }
-                    else {
+                    } else {
                         System.out.println("Quanto você deseja depositar?");
                         int dinheiroDepositar = scanner.nextInt();
                         scanner.nextLine();
@@ -54,18 +54,36 @@ public class Main {
                     }
                     break;
                 case 3:
-                    break;
-                case 4:
-                    for (ArrayList i :acounts){
-                        System.out.println(i);
+                    for (int i = 1; i <= acounts.size(); i++) {
+                        System.out.println(i + ". " + acounts.get(i - 1));
                     }
-                    break;
-                case 5:
-                    break;
-                default:
-                    System.out.println("O Numero que voce digitou na Existe");
-            }
-        }
+                    System.out.println("De qual conta deseja realizar o saque?");
+                    int chosen = scanner.nextInt() - 1;
+                    scanner.nextLine();
+                    System.out.println("Quanto você deseja retirar?");
+                    int dinheiroSaque = scanner.nextInt();
+                    scanner.nextLine();
 
+                    int saldoConta = Integer.parseInt(String.valueOf(acounts.get(chosen).get(1)));
+                    if (saldoConta - dinheiroSaque <= 0) {
+                        System.out.println("Você não possui saldo necessário para extrair");
+                    } else {
+                        saldoConta -= dinheiroSaque;
+                        String novoSaldo = Integer.toString(saldoConta);
+                        acounts.get(chosen).set(1, novoSaldo);
+                    }
+                        break;
+                    case 4:
+                            for (ArrayList i : acounts) {
+                                System.out.println(i);
+                            }
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            System.out.println("O Numero que voce digitou na Existe");
+                    }
+            }
+
+        }
     }
-}
